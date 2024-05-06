dafd4f83c4850f5be62e6245dd0e11ed7fa9d006
thancha.tip <thanchatip.coe@gmail.com>
2024-05-06 16:04:16 +0700

    update gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBaseURL: process.env.API_BASE_URL, // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+    },
+  },
+});
