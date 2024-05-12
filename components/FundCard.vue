5911e0b9cdd2ad3c1013d93d4595ff98b32f025d
thancha.tip <thanchatip.coe@gmail.com>
2024-05-12 21:32:48 +0700

    update data display

--- components/FundCard.vue
+++ components/FundCard.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="fund-card">
     <div class="content-left">
-      <div class="name">{{ data.thailand_fund_code }}</div>
-      <Tag class="nav-date" severity="secondary" rounded>
-        {{ dayjs(data.nav_date).format("DD MMM YYYY") }}
-      </Tag>
-    </div>
-    <div class="content-right">
-      <div class="rank">{{ data.nav_return }}</div>
-
-      <div>
-        {{ data.nav }}
+      <div class="rank" :class="{ 'top-rank': isTopRank }">
+        {{ rank + 1 }}
       </div>
+      <div class="fund-info">
+        <div class="name">
+          {{ data.thailand_fund_code }}
+        </div>
+        <div class="full-name">
+          {{ data.thailand_fund_code }}
+        </div>
+        <Tag class="nav-date" severity="info" rounded>
+          ความเสี่ยง {{ dayjs(data.nav_date).format("DD/MM/YYYY") }}
+        </Tag>
+      </div>
+    </div>
+
+    <div class="content-right">
+      <div class="nav-return">{{ Number(data.nav_return).toFixed(2) }}</div>
+      <div class="nav">{{ Number(data.nav).toFixed(2) }} บาท</div>
+      <Tag class="nav-date" severity="secondary" rounded>
+        ข้อมูล ณ วันที่ {{ dayjs(data.nav_date).format("DD/MM/YYYY") }}
+      </Tag>
     </div>
   </div>
 </template>
@@ -19,27 +30,74 @@
 <script lang="ts" setup>
 import dayjs from "dayjs";
 
+interface FundItem {
+  mstar_id: string;
+  thailand_fund_code: string;
+  nav_return: number;
+  nav: number;
+  nav_date: number;
+  avg_return: number;
+}
+
 interface Props {
-  data?: any;
+  data: FundItem;
+  rank: number;
 }
 
 const props = defineProps<Props>();
+
+const isTopRank = computed(() => {
+  if (props.rank < 3) return true;
+  else return false;
+});
 </script>
 
 <style lang="scss" scoped>
 .fund-card {
-  padding: 10px 20px;
+  padding: 10px 15px;
   background: #ffffff;
   color: #334155;
   border: 1px solid #e2e8f0;
+  box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.1);
   border-radius: 6px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  cursor: pointer;
 }
 
-.content {
-  //flex-direction: coloumn;
+.content-left {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+
+.fund-info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+}
+
+.content-right {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 5px;
+}
+
+.top-rank {
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  font-weight: bold;
+}
+
+.rank {
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  //font-weight: bold;
 }
 
 .name {
@@ -47,7 +105,22 @@ const props = defineProps<Props>();
   font-weight: bold;
 }
 
+.full-name {
+  font-size: 14px;
+  color: #333;
+  //font-weight: bold;
+}
+
 .nav-date {
-  margin-top: 5px;
+  font-size: 10px;
+}
+
+.nav-return {
+  font-weight: bold;
+  color: green;
+}
+
+.nav {
+  font-size: 12px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
+  <div class="range-selector">
+    <div>{{ value }}</div>
+    <SelectButton v-model="value" :options="options" aria-labelledby="basic" />
+  </div>
+  <IconField class="search-input" iconPosition="left">
+    <InputIcon class="pi pi-search"> </InputIcon>
+    <InputText v-model="searchKey" placeholder="Search Fund" />
+  </IconField>
   <div class="grid-container">
-    <div class="fund-list" v-for="fund in funds.data">
-      <FundCard class="col-6" :data="fund" />
+    <div class="fund-list" v-for="(fund, index) in funds.data">
+      <FundCard class="col-6" :data="fund" :rank="index" />
     </div>
 
     <Button label="submit" />
@@ -11,16 +19,35 @@
 <script lang="ts" setup>
 import funds from "~/public/jsonMockups/funds/list-1Y.json";
 
+const value = ref("1Y");
+const options = ref(["1D", "1W", "1M", "1Y"]);
+
+const searchKey = ref("");
+
 onMounted(() => console.log(funds.data));
 </script>
 
 <style lang="scss" scoped>
 .fund-list {
-  margin: 10px 0;
+  margin: 0;
 }
-/* .grid-container {
+.grid-container {
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: auto;
   gap: 10px;
-} */
+}
+
+.search-input {
+  margin-bottom: 20px;
+}
+
+:deep(.p-inputtext) {
+  width: 100%;
+}
+
+.range-selector {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
 </style>
